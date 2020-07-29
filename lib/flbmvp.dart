31f6242038bc0f73618a4f50a3f0ec3ae29b5e41
yangpan <x670990709@126.com>
2020-07-29 10:32:36 +0800

    no message

--- lib/flbmvp.dart
+++ lib/flbmvp.dart
@@ -1,8 +1,8 @@
 library flbmvp;
 
 export 'src/ib_model.dart';
-export 'src/base_model.dart';
-export 'src/ib_presenter.dart';
-export 'src/base_presenter.dart';
 export 'src/ib_view.dart';
+export 'src/ib_presenter.dart';
+export 'src/base_model.dart';
 export 'src/base_view.dart';
+export 'src/base_presenter.dart';
--- lib/src/ib_view.dart
+++ lib/src/ib_view.dart
@@ -1,7 +1,5 @@
 
 abstract class IBView {
-  // 显示加载
   void showLoading();
-  // 隐藏加载
   void hideLoading();
 }
